595acaa7a3ef2f1677c8bfdca5d0c5ddd4711040
Monja <monja.nya@gmail.com>
2020-12-08 15:22:04 +0200

    Implement fractal generation

--- src/components/CanvasComponent.tsx
+++ src/components/CanvasComponent.tsx
@@ -4,19 +4,125 @@ import p5Types from "p5";
 
 interface CanvasComponentProps {
   width: number,
-  height: number
+  height: number,
+  constValue: number,
+  orderValue: number,
+  hueValues: number[]
 }
 
 export const CanvasComponent: React.FC<CanvasComponentProps> = (props: CanvasComponentProps) => {
 
+  const epsilon = 0.001;
+
+  function writeColor(p5: p5Types, img: p5Types.Image, color: p5Types.Color, x: number, y: number) {
+    const index = (x + y * img.width) * 4;
+    img.pixels[index] = p5.red(color);
+    img.pixels[index + 1] = p5.green(color);
+    img.pixels[index + 2] = p5.blue(color);
+    img.pixels[index + 3] = 255 * p5.alpha(color);
+  }
+
   const setup = (p5: p5Types, canvasParentRef: Element) => {
+
+    p5.colorMode(p5.HSL, 360, 100, 100, 1);
     p5.createCanvas(props.width, props.height).parent(canvasParentRef);
+
+    let img = p5.createImage(props.width, props.height);
+    img.loadPixels();
+
+    for (let x = 0; x < img.width; x++) {
+      for (let y = 0; y < img.height; y++) {
+
+        let initialPoint = new Point((x / img.width) * 2 - 1, (y / img.height) * 2 - 2);
+
+        let previousPoint = getNextIteration(initialPoint);
+        let currentPoint = getNextIteration(previousPoint);
+
+        let iteration = 0;
+
+        while (Math.abs(currentPoint.x - previousPoint.x) > epsilon * epsilon) {
+          previousPoint = currentPoint;
+          currentPoint = getNextIteration(currentPoint);
+          iteration++;
+        }
+
+        let pointColor = p5.color(0, 0, 0, 1);
+        let alpha = (iteration / 20) + 0.5;
+
+        if (Math.abs(currentPoint.x - 1) < epsilon) {
+          pointColor = p5.color(props.hueValues[0], 80, 40, alpha);
+        } else {
+          pointColor = currentPoint.y > 0
+            ? p5.color(props.hueValues[1], 80, 40, alpha)
+            : p5.color(props.hueValues[2], 80, 40, alpha);
+        }
+
+        writeColor(p5, img, pointColor, x, y);
+      }
+    }
+
+    img.updatePixels();
+    p5.image(img, 0, 0);
   };
 
   const draw = (p5: p5Types) => {
 
+  }
+
+  const mouseClicked = (p5: p5Types) => {
+    p5.saveCanvas("out.png");
   };
 
-  return <Sketch setup={setup} draw={draw} />;
+  const computeFunction = (point: Point): Point => {
+    return props.orderValue === 3
+      ? point.multiply(point).multiply(point).add(new Point(props.constValue, 0.0))
+      : point;
+  }
+
+  const computeFunctionDerivative = (point: Point): Point => {
+    return props.orderValue === 3
+      ? new Point(3, 0).multiply(point).multiply(point)
+      : point;
+  }
+
+  const getNextIteration = (point: Point): Point => {
+    return point.subtract(computeFunction(point).divide(computeFunctionDerivative(point)));
+  }
+
+  return <Sketch setup={setup} draw={draw} mouseClicked={mouseClicked} />;
 };
+
+class Point {
+
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(point: Point): Point {
+    return new Point(this.x + point.x, this.y + point.y);
+  }
+
+  subtract(point: Point): Point {
+    return new Point(this.x - point.x, this.y - point.y);
+  }
+
+  multiply(point: Point): Point {
+    return new Point(
+      this.x * point.x - this.y * point.y,
+      this.y * point.x + this.x * point.y
+    );
+  }
+
+  divide(point: Point): Point {
+    const denominator = point.x * point.x + point.y * point.y;
+    return new Point(
+      (this.x * point.x + this.y * point.y) / denominator,
+      (this.y * point.x - this.x * point.y) / denominator
+    );
+  }
+}
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import {CanvasComponent} from "./components/CanvasComponent";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <CanvasComponent height={800} width={800} />
-  </React.StrictMode>,
+  <CanvasComponent height={800} width={800} constValue={-1} orderValue={3} hueValues={[10, 120, 280]} />,
   document.getElementById('root')
 );
